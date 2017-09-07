765efecb5eb3f920260e7dbb42b23637d0d66675
gpandcb <truetrue065@gmail.com>
2017-09-07 22:36:45 +0100

    Update Prime_VT.java
    try to add an other chassis

--- FordHR_data/scripts/src/Prime_VT.java
+++ FordHR_data/scripts/src/Prime_VT.java
@@ -11,7 +11,7 @@ public class Prime_VT extends VehicleType
 		prevalence = 0.0;
 
 		// DLH 500 //
-		vmd=new VehicleModel( cars.racers.Prime:0x00000006r, VS_USED );
+		vmd=new VehicleModel( cars.racers.FordHR:0x00000006r, VS_USED );
 		vtdarr.addElement( vmd );
 		vmd.prevalence = 25.0;
 		vmd.stockPrestige=876;	vmd.fullPrestige= 982;
@@ -19,7 +19,7 @@ public class Prime_VT extends VehicleType
 		vmd.vehicleName = "Prime DLH 500";
 		prevalence += vmd.prevalence;
 
-		vmd=new VehicleModel( cars.racers.Prime:0x00000006r, VS_NRACE );
+		vmd=new VehicleModel( cars.racers.FordHR:0x00000006r, VS_NRACE );
 		vtdarr.addElement( vmd );
 		vmd.minPower   = 2.0;	vmd.maxPower	= 2.0;
 		vmd.prevalence = 50.0;
@@ -27,6 +27,23 @@ public class Prime_VT extends VehicleType
 		vmd.stockQM = qm_stock_Prime_DLH_500;	vmd.fullQM = qm_full_Prime_DLH_500;
 		vmd.vehicleName = "Prime DLH 500";
 
+		// DLH 400 //
+		vmd=new VehicleModel( cars.racers.FordHR:0x000000CAr, VS_USED );
+		vtdarr.addElement( vmd );
+		vmd.prevalence = 25.0;
+		vmd.stockPrestige=876;	vmd.fullPrestige= 982;
+		vmd.stockQM = qm_stock_Prime_DLH_400;	vmd.fullQM = qm_full_Prime_DLH_400;
+		vmd.vehicleName = "Prime DLH 400";
+		prevalence += vmd.prevalence;
+
+		vmd=new VehicleModel( cars.racers.FordHR:0x000000CAr, VS_NRACE );
+		vtdarr.addElement( vmd );
+		vmd.minPower   = 2.0;	vmd.maxPower	= 2.0;
+		vmd.prevalence = 50.0;
+		vmd.stockPrestige=876;	vmd.fullPrestige= 982;
+		vmd.stockQM = qm_stock_Prime_DLH_400;	vmd.fullQM = qm_full_Prime_DLH_400;
+		vmd.vehicleName = "Prime DLH 400";
+		
 		// make color indexes //
 		addColorIndex(GameLogic.RID_CARCOLOR_Einvagen_Reflex);
 
